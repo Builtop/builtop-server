2de3d58a35604f7714de5eb7b95e3b0963aaa6c5
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2023-06-01 08:02:12 +0300

    fixing data types

--- managers-auth/src/validation-schemas/reset-password-schema.validation.ts
+++ managers-auth/src/validation-schemas/reset-password-schema.validation.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 import { forgetPasswordMethods } from '../../../common/index';
 
 export const resetPasswordSchema = yup.object().shape({
-    method: yup.mixed().oneOf(Object.values(forgetPasswordMethods)).required(),
+    method: yup.string().oneOf(Object.values(forgetPasswordMethods)).required(),
     phoneNum: yup.string().when('method', values => {
         if(values[0] === forgetPasswordMethods.WithPhoneNumber) {
             return yup.string().matches(/^\+(?:[0-9] ?){6,14}[0-9]$/, 'phone number is not valid').required().ensure();
